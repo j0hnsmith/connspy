58241a0f9106ca546aa194d7d419582e8ba964b7
petems <p.morsou@gmail.com>
2020-03-26 12:12:18 +0000

    Adds new method: NewClientWriter
    * Same as original method but allows you to specify a io.Writer
    * Added an example of how it works, plus a redaction example

--- http/client_test.go
+++ http/client_test.go
@@ -1,7 +1,12 @@
 package http_test
 
 import (
+	"bytes"
+	"fmt"
 	"io/ioutil"
+	"os"
+	"regexp"
+	"strings"
 
 	"github.com/j0hnsmith/connspy/http"
 )
@@ -19,4 +24,72 @@ func ExampleNewClient() {
 	resp.Body.Close()
 
 	// Output:
+}
+
+func ExampleNewClientWriter_toStderr() {
+	client := http.NewClientWriter(nil, nil, os.Stderr)
+
+	resp, _ := client.Get("http://example.com/")
+	// ensure all of the body is read
+	ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+
+	resp, _ = client.Get("https://example.com/")
+	ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+
+	// Output:
+}
+
+func ExampleNewClientWriter_toCustomBuffer() {
+	buf := new(bytes.Buffer)
+
+	client := http.NewClientWriter(nil, nil, buf)
+
+	resp, _ := client.Get("http://example.com/")
+	// ensure all of the body is read
+	ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+
+	resp, _ = client.Get("https://example.com/")
+	ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+
+	httpLog := buf.String()
+
+	s := strings.Split(httpLog, "\n")
+
+	fmt.Println(s[4])
+
+	// Output:
+	// Host: example.com
+}
+
+func ExampleNewClientWriter_toCustomBufferWithRedaction() {
+	buf := new(bytes.Buffer)
+
+	client := http.NewClientWriter(nil, nil, buf)
+
+	resp, _ := client.Get("http://example.com/")
+	// ensure all of the body is read
+	ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+
+	resp, _ = client.Get("https://example.com/")
+	ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+
+	httpLog := buf.String()
+
+	s := strings.Split(httpLog, "\n")
+
+	for count, line := range s {
+		rgx := regexp.MustCompile(`^(Host: )(.+)$`)
+		line = rgx.ReplaceAllString(line, `$1[REDACTED]`)
+		s[count] = line
+	}
+
+	fmt.Println(s[4])
+	// Output:
+	// Host: [REDACTED]
 }
